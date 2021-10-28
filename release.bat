23babf42ce318607290eab02c078e0857373eeba
BenHoule-LPS <benjamin.houle@lawrence.k12.ma.us>
2021-10-28 15:09:19 -0400

    Updated release.bat to name file correctly

--- release.bat
+++ release.bat
@@ -1,3 +1,3 @@
 @echo off
-git archive --format zip --output dists\LPS-PSVault-Resources.zip --worktree-attributes --verbose -9 HEAD
+git archive --format zip --output dists\LPS-SIMSELL.zip --worktree-attributes --verbose -9 HEAD
 pause
